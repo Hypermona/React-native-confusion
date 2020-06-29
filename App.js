07f3950d62c3c762991f026b42136d79d0be0969
Hypermona <mohans8050@gmail.com>
2020-06-29 21:33:21 +0530

    first Commit

--- App.js
+++ App.js
@@ -1,16 +1,8 @@
-import * as React from "react";
-import { View, Text } from "react-native";
+import React from "react";
+import Main from "./components/MainComponent";
 
-export default function App() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Universal React with Expo</Text>
-    </View>
-  );
+export default class App extends React.Component {
+  render() {
+    return <Main />;
+  }
 }
