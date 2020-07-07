94f5bfd450527fa985c34f0daca892f8547bc307
Hypermona <mohans8050@gmail.com>
2020-07-07 11:41:37 +0530

    Gestures

--- components/DishdetailComponent.js
+++ components/DishdetailComponent.js
@@ -1,6 +1,15 @@
 import React, { Component } from "react";
-import { View, Text, ScrollView, FlatList, Modal, Button } from "react-native";
-import { Card, Icon, Rating, Input, AirbnbRating } from "react-native-elements";
+import {
+  View,
+  Text,
+  ScrollView,
+  FlatList,
+  Modal,
+  Button,
+  Alert,
+  PanResponder,
+} from "react-native";
+import { Card, Icon, Rating, Input } from "react-native-elements";
 import { DISHES } from "../shared/dishes";
 import { COMMENTS } from "../shared/comment";
 import { connect } from "react-redux";
@@ -24,9 +33,60 @@ const mapDispatchToProps = (dispatch) => ({
 
 function RenderDish(props) {
   const dish = props.dish;
+  handleViewRef = (ref) => (this.view = ref);
+
+  const recognizeDrag = ({ moveX, moveY, dx, dy }) => {
+    if (dx < -200) return true;
+    else return false;
+  };
+  const panRendponer = PanResponder.create({
+    onStartShouldSetPanResponder: (e, gestureState) => {
+      return true;
+    },
+    onPanResponderGrant: () => {
+      this.view
+        .rubberBand(1000)
+        .then((endState) =>
+          console.log(endState.finished ? "finished" : "cancelled")
+        );
+    },
+    onPanResponderEnd: (e, gestureState) => {
+      console.log("pan responder end", gestureState);
+      if (recognizeDrag(gestureState))
+        Alert.alert(
+          "Add Favorite",
+          "Are you sure you wish to add " + dish.name + " to favorite?",
+          [
+            {
+              text: "Cancel",
+              onPress: () => console.log("Cancel Pressed"),
+              style: "cancel",
+            },
+            {
+              text: "OK",
+              onPress: () => {
+                props.favorite
+                  ? console.log("Already favorite")
+                  : props.onPress();
+              },
+            },
+          ],
+          { cancelable: false }
+        );
+
+      return true;
+    },
+  });
+
   if (dish != null) {
     return (
-      <Animatable.View animation="fadeInDown" duration={2000} delay={1000}>
+      <Animatable.View
+        animation="fadeInDown"
+        duration={2000}
+        delay={1000}
+        ref={this.handleViewRef}
+        {...panRendponer.panHandlers}
+      >
         <Card featuredTitle={dish.name} image={{ uri: baseUrl + dish.image }}>
           <Text style={{ margin: 10 }}>{dish.description}</Text>
           <View style={{ flexDirection: "row", justifyContent: "center" }}>
