e7beca729ee10cd539c1b095a2197d1f860c8d09
Hypermona <mohans8050@gmail.com>
2020-07-08 16:37:09 +0530

    mail composer

--- components/ContactComponents.js
+++ components/ContactComponents.js
@@ -1,9 +1,17 @@
 import React from "react";
 import { View, Text } from "react-native";
-import { Card } from "react-native-elements";
+import { Card, Button, Icon } from "react-native-elements";
 import * as Animatable from "react-native-animatable";
+import * as MailComposer from "expo-mail-composer";
 
 export default function ContactUs() {
+  const sendMail = () => {
+    MailComposer.composeAsync({
+      recipients: ["confusion@food.net"],
+      subject: "Enquiry",
+      body: "To whom it may concern:",
+    });
+  };
   return (
     <View>
       <Animatable.View animation="fadeInDown" duration={2000} delay={1000}>
@@ -14,6 +22,12 @@ export default function ContactUs() {
           <Text>Tel: +852 1234 5678</Text>
           <Text>Fax: +852 8765 4321</Text>
           <Text>Email:confusion@food.net</Text>
+          <Button
+            title="Send Email"
+            buttonStyle={{ backgroundColor: "#512DA8", marginTop: 20 }}
+            icon={<Icon name="envelope-o" type="font-awesome" color="white" />}
+            onPress={sendMail}
+          />
         </Card>
       </Animatable.View>
     </View>
